0d05de3156d36140450ab11b2dfa62eb57f57f69
Ahmet Faruk Turgut <ahmetfturgut@gmail.com>
2020-02-19 02:14:29 +0300

    fix

--- lib/form_islemleri.dart
+++ lib/form_islemleri.dart
@@ -1,20 +1,132 @@
 import 'package:flutter/material.dart';
 
- class FormandTextFormField extends StatefulWidget {
-   @override
-   _FormandTextFormFieldState createState() => _FormandTextFormFieldState();
- }
- 
- class _FormandTextFormFieldState extends State<FormandTextFormField> {
-   @override
-   Widget build(BuildContext context) {
-     return Scaffold(
-       floatingActionButton: FloatingActionButton(onPressed: (){},child: Icon(Icons.save),),
-       appBar: AppBar(
-         title: Text("Form ve TextForm İşlemleri"),
-       ),
-       body: Text("data"),
-       
-     );
-   }
- }
+class FormandTextFormField extends StatefulWidget {
+  @override
+  _FormandTextFormFieldState createState() => _FormandTextFormFieldState();
+}
+
+class _FormandTextFormFieldState extends State<FormandTextFormField> {
+  var _name, _email, _pass, _cins,cinsiyet;
+  var otoControl = false;
+  var termAndConditions = false;
+  final formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData(primaryColor: Colors.indigo, accentColor: Colors.red),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.save),
+        ),
+        appBar: AppBar(
+          title: Text("Form ve TextForm İşlemleri"),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(20),
+          child: Form(
+              key: formKey,
+              autovalidate: otoControl,
+              child: ListView(
+                children: <Widget>[
+                  SizedBox(height: 10),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.account_circle),
+                        hintText: "Adınız ve Soyadınız",
+                        labelText: "Ad Soyad",
+                        border: OutlineInputBorder()),
+                    validator: (var name) {
+                      if (name.length < 6) {
+                        return "Lütfen adınızı soyadınızı tam giriniz";
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (name) => _name = name,
+                  ),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.account_circle),
+                        hintText: "Email Adresiniz",
+                        labelText: "Email",
+                        border: OutlineInputBorder()),
+                    validator: _emailKontrol,
+                    onSaved: (email) => _email = email,
+                  ),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock),
+                        hintText: "Şifreniz",
+                        labelText: "Şifre",
+                        border: OutlineInputBorder()),
+                    validator: (var pass) {
+                      if (pass.length < 6) {
+                        return "En az 6 karakter giriniz";
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (pass) => _pass = pass,
+                  ),
+                  // SizedBox(height: 10),
+                  // RadioListTile(
+                  //     value: "Erkek",
+                  //     groupValue: cinsiyet,
+                  //     onChanged: (cins) => _cins = cins),
+                  // SizedBox(height: 10),
+                  // RadioListTile(
+                  //     value: "Kadın",
+                  //     groupValue: cinsiyet,
+                  //     onChanged: (cins) => _cins = cins),
+                  SizedBox(height: 10),
+                  CheckboxListTile(
+                      title: Text("Okudum Anladım"),
+                      subtitle: Text("Onaylıyorum"),
+                      activeColor: Colors.blue,
+                      value: termAndConditions,
+                      onChanged: (deger) {
+                        setState(() {
+                          termAndConditions = deger;
+                        });
+                      }),
+                  SizedBox(height: 10),
+                  RaisedButton.icon(
+                    icon: Icon(Icons.save),
+                    label: Text("KAYDET"),
+                    color: Colors.blue,
+                    onPressed: _checkLogin,
+                  )
+                ],
+              )),
+        ),
+      ),
+    );
+  }
+
+  void _checkLogin() {
+    if (formKey.currentState.validate() && termAndConditions) {
+      formKey.currentState.save();
+      debugPrint("Şifre: $_pass Ad Soyad: $_name Email: $_email");
+    } else {
+      setState(() {
+        otoControl = true;
+      });
+    }
+  }
+
+  String _emailKontrol(String mail) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(mail))
+      return 'Geçersiz mail';
+    else
+      return null;
+  }
+}
